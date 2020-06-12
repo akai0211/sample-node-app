07350a9f39fd110d8592af2032efcfd9a3b1a40e
Thu Bui <thu@trophyroom.io>
2020-06-12 20:08:53 +0700

    Fix parse error

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const my_name = process.env.JK_NAME
 
 app.get('/', (req, res) => {
-    res.status(200).send({ 'My name is ': + my_name });
+    res.status(200).send({ 'My name is ': my_name });
 });
 
 app.get('/health', (req, res) => {
